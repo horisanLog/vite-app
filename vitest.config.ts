6f387af48d646197dba2d268bd60185bc89f5a69
horisanLog <ttie7374@gmail.com>
2024-01-10 01:10:11 +0900

    test追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vitest/config"
+import { defineConfig, mergeConfig } from "vitest/config"
+import viteConfig from "./vite.config"
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-})
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      include: ["src/tests/**/*.test.tsx"],
+      environment: "jsdom",
+      globals: true, 
+    },
+  })
+)
